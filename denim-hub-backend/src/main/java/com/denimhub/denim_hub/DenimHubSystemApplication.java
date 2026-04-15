adf050651cec4b9bfbf401d23eeb1a3756f344df
NikitaAnilPawar-02 <pawarnikita0293@gmail.com>
2026-04-15 20:35:19 +0530

    Modified application.properties file

--- denim-hub-backend/src/main/java/com/denimhub/denim_hub/DenimHubSystemApplication.java
+++ denim-hub-backend/src/main/java/com/denimhub/denim_hub/DenimHubSystemApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DenimHubSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DenimHubSystemApplication.class, args);
-
         System.out.println("Hello From DenimHubSystemApplication");
 	}
-
 }
